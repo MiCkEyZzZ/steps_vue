5d5ff74773435f7a4a6ea553c96949bcf9f6899e
Михаил Журавлев <mikhail.f.zhuravlev@gmail.com>
2021-03-26 17:04:40 +0500

    created method

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
-  <div class="container">
+  <div class="container pt-5">
     <div class="card">
       <h1>План по изучению Vue.js</h1>
 
       <div class="steps">
         <div class="steps-content">
-          Сюда необходимо вывести текст активного шага по его индексу
+          тест
         </div>
         <ul class="steps-list">
-          <li class="steps-item done"><span>1</span> Шаг 1</li>
-          <li class="steps-item done"><span>2</span> Шаг 2</li>
-          <li class="steps-item active"><span>3</span> Шаг 3</li>
-          <li class="steps-item"><span>4</span> Шаг 4</li>
+          <li
+              class="steps-item"
+              :class="{active: activeIndex === idx, done: activeIndex > idx}"
+              :key="step"
+              v-for="(step, idx) in steps"
+          >
+            <span
+                @click="setActive(idx)"
+            >{{ idx + 1 }}</span> {{ step.title }}</li>
         </ul>
         <div>
           <button
@@ -30,6 +35,22 @@
 
 export default {
   data() {
+    return {
+      activeIndex: 0,
+      isActive: true,
+      steps: [
+        {title: 'Основы', text: 'В блоке вы познакомитесь со всеми основами Vue.js на практике. На протяжении блока мы напишем реактивное приложение, в процессе разработки которого разберем вся базу фреймворка.'},
+        {title: 'Компоненты', text: 'Один из самых важных блоков в курсе, где вы узнаете все о компонентах. В блоке мы напишем 2 разных приложения и создадим более 5 различных UI компонентов как в реальной разработке. Блок расскажет про абсолютно все составляющие, которые есть в компонентах: взаимодействие, slots, асинхронные и динамические компоненты и тонна примеров.'},
+        {title: 'Роутер', text: 'В данном блоке вы узнаете все о том, как работает мультиязычность во Vue. Мы создадим миниклон Gmail в данном блоке, где вы на практике увидите как работать с динамическим роутером.'},
+        {title: 'Vuex', text: 'В блоке вы узнаете абсолютно все про Vuex. Вы узнаете как работать с данными, какие есть лучшие практики по их программированию и структурированию. Все на практике.'},
+        {title: 'Composition', text: 'Одним из наиболее важных обновлений в Vue 3 является появление альтернативного синтаксиса Composition API. В этом блоке вы узнаете все, чтобы полностью пользоваться данными синтаксисом на практических примерах. Помимо этого вы узнаете как работать совместно с Vue Router и Vuex.'},
+      ]
+    }
+  },
+  methods: {
+    setActive(idx) {
+      this.activeIndex = idx
+    }
   }
 }
 </script>
@@ -251,5 +272,9 @@ h3 {
 .steps-item.active span {
   font-weight: bold;
   color: #ffffff;
+}
+
+.pt-5 {
+  padding-top: 5rem;
 }
 </style>
